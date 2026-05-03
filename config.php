93a2b53f4a88eb4e71d93c2d8fd0de223c8f28b5
kholisfathon1310-tec <kholisfathon1310@gmail.com>
2026-05-04 03:35:30 +0700

    hapus semua file

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi=mysqli_connect("localhost","root","","penggajian") or die("Gagal Koneksi Database");
-// echo "Sukses Koneksi database";
-
-$main_url = "http://localhost/penggajian/";
-
-
-?>
--- karyawan.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "config.php";
-require_once "template/header.php";
-require_once "template/sidebar.php";
-require_once "template/footer.php";
-
-?>
